762ddf8c0c0849926d40b96a18eedbaf46cf3715
luo20 <luozheng2002@sjtu.edu.cn>
2023-03-08 23:04:51 +0800

    添加项目文件。

---
+++ TickTackToeModel/TickTackToeModel.cs
@@ -0,0 +1,83 @@
+﻿using Contracts.BaseClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TickTackToe
+{
+	public enum Stone
+	{
+		Empty,
+		Black,
+		White
+	}
+	public class TickTackToeMove : MoveBase
+	{
+		public Stone Stone { get; set; }
+		public int X { get; set; }
+		public int Y { get; set; }
+        public TickTackToeMove(Stone stone, int x, int y)
+        {
+			Stone = stone;
+			X = x;
+			Y = y;
+        }
+    }
+	public class TickTackToeModel: GameModelBase
+	{
+		public Stone[,] Chessboard { get; set; } = new Stone[3, 3];
+		public Stone WhoseTurn { get; set; }
+		public Stone WhoIsAI { get; set;}
+		public static Stone EnemyOf(Stone stone)
+		{
+			if (stone == Stone.Black)
+			{
+				return Stone.White;
+			}
+			else if(stone == Stone.White)
+			{
+				return Stone.Black;
+			}
+			return Stone.Empty;
+		}
+		public override bool ValidMove(MoveBase move)
+		{
+			TickTackToeMove tickTackToeMove = (TickTackToeMove)move;
+			if (Chessboard[tickTackToeMove.Y, tickTackToeMove.X] == Stone.Empty)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		public override void DoMove(MoveBase move)
+		{
+			TickTackToeMove tickTackToeMove = (TickTackToeMove)move;
+			Chessboard[tickTackToeMove.Y, tickTackToeMove.X] = tickTackToeMove.Stone;
+			WhoseTurn = TickTackToeModel.EnemyOf(WhoseTurn);
+		}
+		public override GameModelBase Copy()
+		{
+			TickTackToeModel model = new TickTackToeModel();
+			model.WhoseTurn = WhoseTurn;
+			model.WhoIsAI = WhoIsAI;
+			for(int i = 0;i < 3;i++)
+			{
+				for(int j = 0;j < 3;j++)
+				{
+					model.Chessboard[i,j] = Chessboard[i,j];
+				}
+			}
+			return model;
+		}
+        public TickTackToeModel()
+        {
+			WhoseTurn = Stone.Black;
+        }
+    }
+}
---
+++ TickTackToeTester/TickTackToeTesterClass.cs
@@ -0,0 +1,139 @@
+﻿using Contracts;
+using Contracts.BaseClasses;
+using Emgu.CV;
+using Emgu.CV.ML.MlEnum;
+using Emgu.CV.Structure;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using TickTackToe;
+using TickTackToeTester.Properties;
+namespace TickTackToeTester
+{
+	public class TickTackToeTesterClass: TesterBase
+	{
+		public TickTackToeModel Model { get; } = new TickTackToeModel();
+		Image<Bgr, byte> Image { get; set; } = new Image<Bgr, byte>(30, 30);
+		private void PutPixel(int x, int y, byte r, byte g, byte b)
+		{
+			Image.Data[y, x, 0] = b;
+			Image.Data[y, x, 1] = g;
+			Image.Data[y, x, 2] = r;
+		}
+		//private void FillRectangle(int x1, int y1, int x2,  int y2, byte r, byte g, byte b)
+		//{
+		//	for(int i = y1; i < y2;i++)
+		//	{
+		//		for(int j = x1; j < x2;j++)
+		//		{
+		//			PutPixel(i, j, r, g, b);
+		//		}
+		//	}
+		//}
+		private void DrawSprite(Image<Bgr, byte> sprite, int x, int y)
+		{
+			byte[,,] data = sprite.Data;
+			int rowCount = data.GetLength(0);
+			int columnCount = data.GetLength(1);
+			for (int i = 0; i < rowCount; i++)
+			{
+				for (int j = 0; j < columnCount; j++)
+				{
+					PutPixel(x + j, y + i, data[j, i, 2], data[j, i, 1], data[j, i, 0]);
+				}
+			}
+		}
+		private void DrawBoard()
+		{
+			Image = Resources.board.ToImage<Bgr, byte>();
+			for(int i = 0;i < 3;i++)
+			{
+				for(int j = 0;j < 3;j++)
+				{
+					if (Model.Chessboard[i,j] == Stone.Black)
+					{
+						DrawSprite(Resources.black.ToImage<Bgr, byte>(), 108*j + 4, 108*i + 4);
+					}
+					else if (Model.Chessboard[i,j] == Stone.White)
+					{
+						DrawSprite(Resources.white.ToImage<Bgr, byte>(), 108 * j + 4, 108 * i + 4);
+					}
+				}
+			}
+		}
+		public override void Init()
+		{
+			DrawBoard();
+			UpdateImage(Image);
+		}
+		public override void OnLeftButtonDown(double x, double y)
+		{
+			int realX = (int)(x * 3.0);
+			int realY = (int)(y * 3.0);
+			if (realX > 2 || realY > 2)
+			{
+				return;
+			}
+			TickTackToeMove move = new TickTackToeMove(Model.WhoseTurn, realX, realY);
+			bool validMove = Model.ValidMove(move);
+			if (validMove)
+			{
+				Model.DoMove(move);
+				SendMessage($"成功执行了开发者的落子({move.Stone}, {realX}, {realY})");
+				DrawBoard();
+				UpdateImage(Image);
+				try
+				{
+					MoveInfo info = ProjSlnFuncProvider!.Execute(Model);
+					if (info.Succeeded)
+					{
+						SendMessage($"AI落子成功");
+						TickTackToeMove aiMove = (TickTackToeMove)info.Move!;
+						SendMessage($"AI的落子: ({aiMove.Stone}, {aiMove.X}, {aiMove.Y})");
+						if (Model.ValidMove(aiMove))
+						{
+							Model.DoMove(aiMove);
+							DrawBoard();
+							UpdateImage(Image);
+							SendMessage($"AI落子合规");
+						}
+						else
+						{
+							SendMessage("错误: AI落子不合规");
+						}
+					}
+					else
+					{
+						SendMessage("AI无法决策如何落子");
+					}
+				}
+				catch (Exception e)
+				{
+					SendMessage("调用AI时引发的异常: " + e.Message);
+				}
+			}
+			else
+			{
+				SendMessage("非法的移动");
+			}
+		}
+		public override void OnRightButtonDown(double x, double y)
+		{
+			throw new NotImplementedException();
+		}
+		public override void OnReceiveMessage(string message)
+		{
+			SendMessage($"收到了指令: {message}");
+			if (message == "clear")
+			{
+				Model.Chessboard = new Stone[3, 3];
+				DrawBoard();
+				UpdateImage(Image);
+				SendMessage("收到了重置指令");
+			}
+		}
+		public TickTackToeTesterClass()
+        {
+            
+        }
+    }
+}
